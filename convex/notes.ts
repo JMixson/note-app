a048249e5c70175d951bdc53ac8700e6afa31632
Jasmine Mixson <jmixson1179@gmail.com>
2025-09-15 23:40:01 -0700

    Add createNote mutation function

--- convex/notes.ts
+++ convex/notes.ts
@@ -1,8 +1,26 @@
-import { query } from "./_generated/server";
+import { query, mutation } from "./_generated/server";
+import { v } from "convex/values";
 
 export const get = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query("notes").collect();
+  },
+});
+
+export const createNote = mutation({
+  args: {
+    title: v.string(),
+    author: v.string(),
+    content: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const noteId = await ctx.db.insert("notes", {
+      title: args.title,
+      author: args.author,
+      content: args.content,
+      updatedTime: Date.now(),
+    });
+    return noteId;
   },
 });
