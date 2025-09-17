39aa2539227d5770807621b4808d8c0311897187
Jasmine Mixson <jmixson1179@gmail.com>
2025-09-17 00:22:06 -0700

    Update card layout

--- src/app/components/NoteCard.tsx
+++ src/app/components/NoteCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { useQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
 import { Id } from "../../../convex/_generated/dataModel";
@@ -8,12 +9,21 @@ function NoteCard({ id }: { id: Id<"notes"> }) {
   const note = useQuery(api.notes.getNote, { id });
 
   return (
-    <div className="max-w-sm overflow-hidden rounded shadow-lg">
-      <div className="px-6 py-4">
-        <div className="mb-2 text-xl font-bold">{note?.title}</div>
-        <p className="text-base text-gray-700">{note?.content}</p>
-      </div>
-    </div>
+    <Link
+      href={`/notes/${id}`}
+      className="block max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow-sm hover:bg-gray-100"
+    >
+      <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+        {note?.title}
+      </h3>
+      <h4 className="mb-2 font-bold tracking-tight text-gray-900">
+        By: {note?.author}
+      </h4>
+      <p className="font-normal text-gray-700">{note?.content}</p>
+      <p className="mt-1 font-normal text-gray-700">
+        {new Date(note?._creationTime || "").toLocaleDateString()}
+      </p>
+    </Link>
   );
 }
 
