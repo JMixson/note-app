774e7e5224de442e632fd38f700b41045d7f116e
Jasmine Mixson <jmixson1179@gmail.com>
2025-09-17 00:05:55 -0700

    Update layout for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
+import Image from "next/image";
 import NoteForm from "./components/NoteForm";
 import NoteList from "./components/NoteList";
+import CommunityImage from "../assets/img/community-group.jpg";
 
 function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <NoteForm />
+    <div className="mx-auto my-10 max-w-screen-xl px-4 sm:px-6 lg:px-8">
+      <main>
+        <h1 className="mb-2 text-4xl font-bold">Community Notes Center</h1>
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
+          <Image
+            src={CommunityImage}
+            alt="Community group"
+            className="rounded-md"
+          />
+          <div>
+            <h2 className="text-2xl font-bold">Add a Note!</h2>
+            <NoteForm />
+          </div>
+        </div>
+
         <NoteList />
       </main>
     </div>
