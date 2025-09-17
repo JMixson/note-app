26e373844a57db005195666a26fb1e588ea92ebb
Jasmine Mixson <jmixson1179@gmail.com>
2025-09-17 00:27:59 -0700

    Add photo credit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,21 @@ function Home() {
       <main>
         <h1 className="mb-2 text-4xl font-bold">Community Notes Center</h1>
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
-          <Image
-            src={CommunityImage}
-            alt="Community group"
-            className="rounded-md"
-          />
+          <div>
+            <Image
+              src={CommunityImage}
+              alt="Community group"
+              className="rounded-md"
+            />
+            <p className="text-sm underline hover:no-underline">
+              <a
+                target="_blank"
+                href="https://www.pexels.com/photo/group-of-people-standing-inside-room-2608517/"
+              >
+                Photo by Matheus Bertelli
+              </a>
+            </p>
+          </div>
           <div>
             <h2 className="text-2xl font-bold">Add a Note!</h2>
             <NoteForm />
