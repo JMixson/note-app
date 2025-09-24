9fe0da1a6dc9c364cf4fbbb9f33ac1d1f5302dc3
Jasmine Mixson <jmixson1179@gmail.com>
2025-09-23 23:09:59 -0700

    Set limits on get all notes query

--- convex/notes.ts
+++ convex/notes.ts
@@ -3,9 +3,12 @@ import { query, mutation } from "./_generated/server";
 import { v } from "convex/values";
 
 export const get = query({
-  args: {},
-  handler: async (ctx) => {
-    return await ctx.db.query("notes").order("desc").collect();
+  args: { limit: v.optional(v.number()) },
+  handler: async (ctx, args) => {
+    return await ctx.db
+      .query("notes")
+      .order("desc")
+      .take(args.limit || 50);
   },
 });
 
