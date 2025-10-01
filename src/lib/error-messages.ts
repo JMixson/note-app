73b1d95f572dcab9f77e9541f8ae75f50be2e901
Jasmine Mixson <jmixson1179@gmail.com>
2025-10-01 00:05:11 -0700

    Create general error messages file

---
+++ src/lib/error-messages.ts
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGES = {
+  NOTE_NOT_FOUND: "Note does not exist",
+  UNAUTHORIZED: "You are not authorized to perform this action",
+  USER_NOT_FOUND: "User does not exist",
+  UNKNOWN: "An unknown error occurred",
+} as const;
